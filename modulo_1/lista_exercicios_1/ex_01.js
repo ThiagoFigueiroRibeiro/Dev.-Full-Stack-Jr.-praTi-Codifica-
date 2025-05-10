ceae84a5c74b92f95645e89b8fb2f908bf704c72
Thiago Figueiró Ribeiro <thiago.figueiro.ribeiro@gmail.com>
2025-05-10 20:53:03 -0300

    Ex 2 feito. Pequena correção ex 1.

--- modulo_1/lista_exercicios_1/ex_01.js
+++ modulo_1/lista_exercicios_1/ex_01.js
@@ -1,8 +1,11 @@
 // 1. Escreva um programa que recebe um número inteiro e verifica se ele é par ou ímpar 
 // utilizando uma estrutura de controle if.
+// Node.js v22.15.0.
 // ATENÇÂO: VocÊ vai precisar instalar o "prompt" pra o código funcionar: npm install prompt-sync
 
 const prompt = require('prompt-sync')();
 const number = prompt("Escreva um número inteiro: ");
 
-if (number%2 == 0){console.log("O número é par")} else {console.log("O número é impar")}
+if (number%2 == 0){console.log("O número é par")} 
+else if (number%2 != 0) {console.log("O número é impar")}
+else {console.log("Algo deu errado. Tente novamente mais tarde.")}
---
+++ modulo_1/lista_exercicios_1/ex_02.js
@@ -0,0 +1,15 @@
+//  2. Crie um programa que classifica a idade de uma pessoa em categorias (criança,
+//  adolescente, adulto, idoso) com base no valor fornecido, utilizando uma estrutura de
+//  controle if-else.
+// Node.js v22.15.0.
+// ATENÇÂO: VocÊ vai precisar instalar o "prompt" pra o código funcionar: npm install prompt-sync
+
+const prompt = require('prompt-sync')();
+const number = prompt("Qual é a idade da pessoa: ");
+
+if (number <= 10){console.log("Essa pessoa é uma criança.")}
+else if (number <= 18 && number > 10){console.log("Essa pessoa é um adolescente.")}
+else if (number <= 60 && number > 18){console.log("Essa pessoa é um adulto.")}
+else if (number <= 120 && number > 60){console.log("Essa pessoa é um adulto.")}
+else if (number > 120){console.log("Essa pessoa é um matusalém!")}
+else {console.log("Algo deu errado. Tente novamente mais tarde.")}
